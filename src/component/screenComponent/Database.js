2a77efaac40f3201d143ad4f06f757b34204aec1
Priya Kumari <priyajnv1999@gmail.com>
2023-02-22 17:36:51 +0530

    add new files

---
+++ src/component/screenComponent/Database.js
@@ -0,0 +1,34 @@
+export const videoData = [
+    {
+        video: require(''),
+        postProfile: require(''),
+        title:'Ram_Charan',
+        description:'Feel the beauty of nature',
+        likes:'245',
+        isLike:false,
+    },
+    {
+        video: require(''),
+        postProfile: require(''),
+        title:'Ram_Charan',
+        description:'Feel the beauty of nature',
+        likes:'245',
+        isLike:false,
+    },
+    {
+        video: require(''),
+        postProfile: require(''),
+        title:'Ram_Charan',
+        description:'Feel the beauty of nature',
+        likes:'245',
+        isLike:false,
+    },
+    {
+        video: require(''),
+        postProfile: require(''),
+        title:'Ram_Charan',
+        description:'Feel the beauty of nature',
+        likes:'245',
+        isLike:false,
+    },
+]
--- src/component/screens/Activity.js
+++ src/component/screens/Activity.js
@@ -1,10 +1,27 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { ScrollView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
 const Activity = () => {
   return (
-    <View>
-      <Text>Activity</Text>
+    <View style={{
+      width: '100%',
+      height: '100%',
+      backgroundColor: 'white'
+    }}>
+      <Text style={{
+        fontSize: 20,
+        fontWeight: 'bold',
+        borderBottomWidth: 0.5,
+        borderBottomColor: '#DEDEDE',
+        padding: 10
+      }}>
+        Activity
+        </Text>
+        <ScrollView style={{margin:10}}>
+          <Text style={{fontWeight:'bold'}}>
+            This Week
+          </Text>
+        </ScrollView>
     </View>
   )
 }
